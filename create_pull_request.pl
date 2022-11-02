c799b299061a4297bddd42b626065d686b21640b
Bobby McCann <bobby.mccann@spareroom.co.uk>
2022-11-02 15:47:01 +0000

    Fixes

--- create_pull_request.pl
+++ create_pull_request.pl
@@ -1,8 +1,13 @@
 #!/usr/bin/perl
 use strict;
 use warnings;
+use 5.20.0;
 use URI::Encode qw(uri_encode uri_decode);
 
-my $branch = uri_encode `git branch --show-current`;
+my $encoder = URI::Encode->new({encode_reserved => 1});
+my $branch = `git branch --show-current`;
+chomp $branch;
+$branch = $encoder->encode($branch);
+
 my $url = "https://github.com/SpareRoom/secure/compare/$branch?expand=1";
 `open $url`;
